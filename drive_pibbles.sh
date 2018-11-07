71c48215b50df0d76516f44e202ee1056bfd1949
Zaid Khan <brain@zaidkhan.me>
2018-11-07 11:13:51 -0600

    Drive script now runs over all predicted images.

--- drive_pibbles.sh
+++ drive_pibbles.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env expect
 set images [glob -directory "predicted_images" -- "*.jpg"]
+spawn ./darknet pibbles cfg/yolov3.cfg yolov3.weights
 foreach image $images {
   puts "predicted_images/$image"
+  expect "Enter Image Path:"
+  send "predicted_images/$image"
+  expect "Person"
 }
-spawn ./darknet pibbles cfg/yolov3.cfg yolov3.weights
-
-expect "Enter Image Path:"
 send "data/dog.jpg\r"
-expect "Person"
